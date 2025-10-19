af6f7d6556bca916050bc5b1670e8d98a06b7de8
alphamalejanghoyoung <ghdud0474@gmail.com>
2025-10-19 18:08:32 +0900

    feat(view) : 클래스 이름 변경, InputView 클래스 기능 추가
    - Input -> InputView
    - Output -> OutputView
    - InputView : Console.readLine 추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,9 @@
+package view;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String inputLine() {
+        System.out.print("덧셈할 문자열을 입력해 주세요: ");
+        return Console.readLine();
+    }
+}
